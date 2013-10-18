920567b72e8f17dc027707e06efeaff9a0dcc625
Cori Allen <coriallen@gmail.com>
2013-10-18 12:12:49 -0400

    Created rails app.

---
+++ code/polymorphic_example/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PolymorphicExample::Application.config.secret_key_base = 'e4f8dd5041d8169bbd80e8d3b1f4d988ea592264449137e4751eacd5d1ecf7d79f2fc42148f77fdf23f8c07f9820c343ddc2f33505424ceb7c00d43709fbd873'
